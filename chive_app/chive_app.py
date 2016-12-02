5a82d2d8c500257a437b2bf78f0b56486ac3cbcb
3pings <jbarksdale@threepings.com>
2016-12-02 07:39:33 -0500

    Added cleaner API

--- chive_app/chive_app.py
+++ chive_app/chive_app.py
@@ -38,4 +38,4 @@ class getInventory(Resource):
         return DEVICES
 
 if __name__ == '__main__':
-app.run(debug=False, host='0.0.0.0', port=5000)
+ app.run(debug=False, host='0.0.0.0', port=5000)
